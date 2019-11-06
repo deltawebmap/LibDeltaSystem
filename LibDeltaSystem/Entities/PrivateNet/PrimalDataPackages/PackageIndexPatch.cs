b562b2ae583084c9b1e28990c9f4b32a0115cae1
Roman-Port <darkwings220@gmail.com>
2019-11-06 16:48:34 -0600

    Fixed a problem that made a couple files not sync to git. Thanks to Scarecrow #7165 on the Discord for pointing this out to me!

---
+++ LibDeltaSystem/Entities/PrivateNet/PrimalDataPackages/PackageIndexPatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibDeltaSystem.Entities.PrivateNet.Packages
+{
+    public class PackageIndexPatch
+    {
+        public string sha1;
+        public string name;
+        public DateTime time;
+        public string patch;
+        public string url;
+        public string id;
+    }
+}
