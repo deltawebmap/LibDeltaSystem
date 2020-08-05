b8379d34c54d09bc0e286b26560acfc4da4b4ecc
RomanPort <34401219+Roman-Port@users.noreply.github.com>
2020-08-04 23:40:41 -0500

    Rolled back last hotfix

--- LibDeltaSystem/DeltaConnection.cs
+++ LibDeltaSystem/DeltaConnection.cs
@@ -27,7 +27,7 @@ namespace LibDeltaSystem
     public class DeltaConnection : DeltaDatabaseConnection
     {
         public const byte LIB_VERSION_MAJOR = 0;
-        public const byte LIB_VERSION_MINOR = 12;
+        public const byte LIB_VERSION_MINOR = 13;
 
         public DeltaConnectionConfig config;
         public HttpClient http;
